2190a19defa8548dcaa59605a59baa2f9f679290
Ondra Machacek <omachace@redhat.com>
2019-01-04 13:05:14 +0100

    Add compare resource filter

--- filter_plugins/ovirtfilters.py
+++ filter_plugins/ovirtfilters.py
@@ -1,13 +1,45 @@
 #!/usr/bin/python
+
+from ansible.module_utils._text import to_native
+
 class FilterModule(object):
     def filters(self):
         return {
+            # IP filters:
             'ovirtvmip': self.ovirtvmip,
             'ovirtvmips': self.ovirtvmips,
             'ovirtvmipv4': self.ovirtvmipv4,
             'ovirtvmipsv4': self.ovirtvmipsv4,
             'ovirtvmipv6': self.ovirtvmipv6,
             'ovirtvmipsv6': self.ovirtvmipsv6,
+
+            # Compare filters:
+            'ovirtdiff': self.ovirtdiff,
+        }
+
+    def ovirtdiff(self, vm1, vm2):
+        """
+        This filter takes two dictionaries of two different resources and compare
+        them. It return dictionari with keys 'before' and 'after', where 'before'
+        containes old values of resources and 'after' contains new values.
+
+        This is mainly good to compare current VM object and next run VM object to see
+        the difference for the next_run.
+        """
+        before = []
+        after = []
+        if vm1.get('next_run_configuration_exists'):
+            keys = [
+                key for key in set(list(vm1.keys()) + list(vm2.keys()))
+                if (key in vm1 and (not key in vm2 or vm2[key] != vm1[key])) or (key in vm2 and (not key in vm1 or vm1[key] != vm2[key]))
+            ]
+            for key in keys:
+                before.append((key, vm1.get(key)))
+                after.append((key, vm2.get(key, vm1.get(key))))
+
+        return {
+            'before': dict(before),
+            'after': dict(after),
         }
  
     def ovirtvmip(self, ovirt_vms, attr=None):
